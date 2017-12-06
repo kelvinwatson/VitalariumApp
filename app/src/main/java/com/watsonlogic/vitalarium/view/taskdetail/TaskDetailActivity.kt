02d39f8427f8a18823e970d68f7a4f3ad9c8840a
Watson,Kelvin <kelvincswatson@gmail.com>
2017-12-06 18:51:34 -0500

    task detail form

--- app/src/main/java/com/watsonlogic/vitalarium/view/taskdetail/TaskDetailActivity.kt
+++ app/src/main/java/com/watsonlogic/vitalarium/view/taskdetail/TaskDetailActivity.kt
@@ -3,6 +3,8 @@ package com.watsonlogic.vitalarium.view.taskdetail
 import android.os.Bundle
 import android.os.Parcelable
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import android.widget.Toolbar
 
 import com.watsonlogic.vitalarium.R
 import com.watsonlogic.vitalarium.model.task.Task
@@ -15,12 +17,24 @@ class TaskDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_detail)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) //show back button
         fragmentManager.beginTransaction().replace(R.id.task_detail_fragment_container,
                 TaskDetailFragment.newInstance(intent.getParcelableExtra<Parcelable>(EXTRA_TASK) as Task),
-                TAG_FRAGMENT).commit()
+                TAG_FRAGMENT).addToBackStack(TAG_FRAGMENT).commit()
     }
 
     companion object {
         private val TAG_FRAGMENT = "TaskDetailFragment"
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            android.R.id.home -> {
+                // app icon in action bar clicked; goto parent activity.
+                this.finish()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/com/watsonlogic/vitalarium/view/taskdetail/TaskDetailFragment.java
+++ app/src/main/java/com/watsonlogic/vitalarium/view/taskdetail/TaskDetailFragment.java
@@ -1,9 +1,14 @@
 package com.watsonlogic.vitalarium.view.taskdetail;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 import com.watsonlogic.vitalarium.R;
 import com.watsonlogic.vitalarium.model.task.Task;
@@ -12,6 +17,9 @@ import static com.watsonlogic.vitalarium.view.dashboard.DashboardActivity.EXTRA_
 
 public class TaskDetailFragment extends android.app.Fragment {
     private Task task;
+    private String[] sizes = new String[]{"Small", "Medium", "Large"};
+    private String[] sprints = new String[]{"Backlog", "Current Sprint", "Next Sprint"};
+    private String[] statuses = new String[]{"Not Started", "In Progress", "Done"};
 
     public TaskDetailFragment() {
         // Required empty public constructor
@@ -31,11 +39,29 @@ public class TaskDetailFragment extends android.app.Fragment {
         if (getArguments() != null) {
             task = getArguments().getParcelable(EXTRA_TASK);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_task_detail, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View v, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(v, savedInstanceState);
+        Spinner sizeSpinner = v.findViewById(R.id.task_size_spinner);
+        SpinnerAdapter sizeSpinnerAdapter = new ArrayAdapter<>(v.getContext(),
+                android.R.layout.simple_spinner_dropdown_item, sizes);
+        sizeSpinner.setAdapter(sizeSpinnerAdapter);
+
+        Spinner sprintSpinner = v.findViewById(R.id.task_sprint_spinner);
+        SpinnerAdapter sprintSpinnerAdapter = new ArrayAdapter<>(v.getContext(), android.R.layout.simple_spinner_dropdown_item, sprints);
+        sprintSpinner.setAdapter(sprintSpinnerAdapter);
+
+        Spinner statusSpinner = v.findViewById(R.id.task_status_spinner);
+        SpinnerAdapter statusSpinnerAdapter = new ArrayAdapter<>(v.getContext(), android.R.layout.simple_spinner_dropdown_item, statuses);
+        statusSpinner.setAdapter(statusSpinnerAdapter);
     }
 }
